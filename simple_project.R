0a690af4b597900d95aff281170456b74a73dc80
Soumya Banerjee <neel.soumya@gmail.com>
2022-07-06 12:27:37 +0100

    Update simple_project.R

--- simple_project.R
+++ simple_project.R
@@ -40,3 +40,27 @@ str(housing)
 # TODO: Density plot of Median Value of house prices
 
 
+############################
+# Creating train-test sets
+############################
+
+set.seed(387)
+train.idx = createDataPartition(y = housing$medv, p = 0.75, list = FALSE)
+test.idx =createDataPartition(y=housing$medv, p=0.25,list=FALSE)
+train = housing[train.idx, ]
+test = housing[test.idx, ]
+
+# The createDataPartition() returns random index numbers to select for the train and test set. The rows corresponding to these indexes are used to obtain the ‘train’ and ‘test’ set.
+
+######################
+# Building the model
+######################
+
+model = lm( medv ~ crim + rm + tax + lstat, data = train)
+
+# The lm() function creates a linear model. In the first argument, variables on left of ‘~’ is the target variable and on the right are independent variables.
+
+
+summary(model) # Obtain coefficients, Residuals and statistics
+rsquare = summary(model)$r.squared # R-squared value
+
